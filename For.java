cccc279cf4e33ee92464b9e84b69e5bc07496085
abhinavvashishtha10 <abhinavvashitha10@gmail.com>
2023-01-30 13:41:06 +0000

    for loop

---
+++ For.java
@@ -0,0 +1,14 @@
+package jan2023;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		for(num=10;num>0;num--)
+		{
+			System.out.println("welcome to java programming :" +num);
+		}
+	}
+
+}
