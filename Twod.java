56be6176be8e6b961c661b3cd427ccad34005823
abhinavvashishtha10 <abhinavvashitha10@gmail.com>
2023-01-30 14:29:18 +0000

    two d array

---
+++ Twod.java
@@ -0,0 +1,25 @@
+package jan2023;
+
+public class Twod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int twodarr[] [];
+		twodarr =new int[4][5];
+		int k=0;
+		for (int i=0; i<4;i++)
+		{
+			for (int j=0; j>5; j++)
+		
+			
+		{
+		
+
+twodarr[i][j]=k;
+System.out.println("value of twodarr["+i+"] ["+j+"]is :"+twodarr[i][j]);
+k++;
+		}
+			System.out.println("******");
+	}
+	}
+}
