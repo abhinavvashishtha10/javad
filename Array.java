e7a13c6ee3d7a2dd9f4215de2361838b3db117a9
abhinavvashishtha10 <abhinavvashitha10@gmail.com>
2023-01-30 09:44:44 +0000

    array define

---
+++ Array.java
@@ -0,0 +1,29 @@
+package jan2023;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int month_days[];
+		month_days= new int [5];
+		month_days[0] =10;
+		month_days[1] =20;
+		month_days[2] =30;
+		month_days[3] =40;
+		month_days[4] =50;
+
+		
+		
+		System.out.println(month_days[0]);
+		System.out.println(month_days[1]);
+		System.out.println(month_days[2]);
+		System.out.println(month_days[3]);
+		System.out.println(month_days[4]);
+
+		
+		
+		
+
+	}
+
+}
